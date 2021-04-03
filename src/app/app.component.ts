f9acead59025fb29ee40c8e6fc4abfc90c2cb90a
Shankar <212604198@ge.com>
2021-04-03 12:21:59 +0530

    Commiting changes for APP_INITIALIZATION

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-practice-second';
+
+  constructor(private http: HttpClient) {
+    console.log('Inside App component\'s constructor');
+    this.http.get('https://api.github.com/users/mike-north').subscribe((resp) => {
+      console.log('Response 2 - ', resp);
+    });
+  }
+
+  ngOnInit() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, APP_INITIALIZER, DoBootstrap } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { delay } from 'rxjs/operators';
 
 import { AppComponent } from './app.component';
+/*
+export function initApp(http: HttpClient) {
+  return () => {
+    return http.get('https://api.github.com/users/VishwanathShankar')
+      .toPromise()
+      .then((resp) => {
+        console.log('Response 1 - ', resp);
+      });
+  };
+}
+*/
+  export function initApp(http: HttpClient) {
+    return () => {
+      return new Promise((resolve) => {
+             setTimeout(() => {
+               console.log('In initApp');
+               resolve("testData");
+             }, 3000);
+           });
+    };
+  }
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: APP_INITIALIZER,
+    useFactory: initApp,
+    multi: true,
+    deps: [HttpClient]
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
